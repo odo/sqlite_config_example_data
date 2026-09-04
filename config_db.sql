fa21a453ac3b4b97172f8f588cac093d2425f3d4
Florian Odronitz <odo@mac.com>
2026-09-04 17:04:14 +0200

    second item

--- config_db.sql
+++ config_db.sql
@@ -1,2 +1,4 @@
 CREATE TABLE config_items ( id   INTEGER PRIMARY KEY, name TEXT);
 INSERT INTO config_items (name) VALUES ('hello world');
+INSERT INTO config_items (name) VALUES ('hello schnoerld');
+
